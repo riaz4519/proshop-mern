36da60dd7860f5e9fe227e3b63681527066799e3
riaz4519 <riaz.i3216@gmail.com>
2022-01-26 18:51:11 +0600

    router 6 setup done adding product page next

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,26 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
+import Product from "./components/Product";
 const App = () => {
   return (
-    <>
-      <h1>welcone to proshop</h1>
-    </>
+    <Router>
+      <Header />
+
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+
+      <Footer />
+    </Router>
   );
 };
 
