0de404687f5cf56d89491a349531ae740d08892a
riaz4519 <riaz.i3216@gmail.com>
2022-01-30 22:31:43 +0600

    on redux product requst

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import colors from "colors";
 import productRoutes from "./routes/productRoutes.js";
 
@@ -21,6 +21,11 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+
+//error
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,10 @@
+import {
+  PRODUUCT_LIST_REQUEST,
+  PRODUUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+} from "../constants/productConstant";
+
+export const listProducts = () => async (dispatch) => {
+
+
+}
